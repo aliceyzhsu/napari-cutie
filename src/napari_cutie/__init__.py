af82cb60c3dba56474c7f133ae6692101ce7510b
Alice Hsu <alice.yz.hsu@outlook.com>
2024-07-17 20:33:41 +0800

    implement tracking from first frame (#1)
    Co-authored-by: alice.yz.hsu <118011772+AliceXuYuanzhen@users.noreply.github.com>

--- src/napari_cutie/__init__.py
+++ src/napari_cutie/__init__.py
@@ -6,7 +6,7 @@ except ImportError:
 
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
+from ._widget import cutie_track_widget
 from ._writer import write_multiple, write_single_image
 
 __all__ = (
@@ -14,8 +14,5 @@ __all__ = (
     "write_single_image",
     "write_multiple",
     "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
+    "cutie_track_widget"
 )
--- src/napari_cutie/_reader.py
+++ src/napari_cutie/_reader.py
@@ -1,35 +1,26 @@
 """
-This module is an example of a barebones numpy reader plugin for napari.
-
-It implements the Reader specification, but your plugin may choose to
-implement multiple readers or even other plugin contributions. see:
-https://napari.org/stable/plugins/guides.html?#readers
+This reader receive the path to a .npy file (annotation of a single frame)
+The naming of .npy file is supposed to be cellpose-styled.
+And return a label layer according to the index number
 """
+import re
+
 import numpy as np
+import napari
+from napari.utils.notifications import show_error
 
 
 def napari_get_reader(path):
-    """A basic implementation of a Reader contribution.
-
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    function or None
-        If the path is a recognized format, return a function that accepts the
-        same path or list of paths, and returns a list of layer data tuples.
-    """
     if isinstance(path, list):
-        # reader plugins may be handed single path, or a list of paths.
-        # if it is a list, it is assumed to be an image stack...
-        # so we are only going to look at the first file.
-        path = path[0]
+        return None
 
-    # if we know we cannot read the file, we immediately return None.
-    if not path.endswith(".npy"):
+    if not path.endswith("_seg.npy"):
+        show_error('it is not a cellpose_styled seg file')
+        return None
+
+    active_layer = napari.current_viewer().layers.selection.active
+    if active_layer is None or not isinstance(active_layer, napari.layers.Image):
+        show_error('current layer is not an Image layer')
         return None
 
     # otherwise we return the *function* that can read ``path``.
@@ -37,36 +28,15 @@ def napari_get_reader(path):
 
 
 def reader_function(path):
-    """Take a path or list of paths and return a list of LayerData tuples.
+    data = np.load(path, allow_pickle=True).item()['masks']
+    match = re.search(r'(\d+)_seg.npy', path)
+    img = napari.current_viewer().layers.selection.active.data
+    labels = np.zeros_like(img)
+    index = int(match.groups()[0])
+    labels[index] = data
+    name = f'initial_annotation_{match.groups()[0]}'
+    add_kwargs = {"name": name}
 
-    Readers are expected to return data as a list of tuples, where each tuple
-    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
-    both optional.
+    layer_type = "labels"
+    return [(labels, add_kwargs, layer_type)]
 
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    layer_data : list of tuples
-        A list of LayerData tuples where each tuple in the list contains
-        (data, metadata, layer_type), where data is a numpy array, metadata is
-        a dict of keyword arguments for the corresponding viewer.add_* method
-        in napari, and layer_type is a lower-case string naming the type of
-        layer. Both "meta", and "layer_type" are optional. napari will
-        default to layer_type=="image" if not provided
-    """
-    # handle both a string and a list of strings
-    paths = [path] if isinstance(path, str) else path
-    # load all files into array
-    arrays = [np.load(_path) for _path in paths]
-    # stack arrays into single array
-    data = np.squeeze(np.stack(arrays))
-
-    # optional kwargs for the corresponding viewer.add_* method
-    add_kwargs = {}
-
-    layer_type = "image"  # optional, default is "image"
-    return [(data, add_kwargs, layer_type)]
